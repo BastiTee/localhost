d5f1330117c1f65f8dd9b0359ee50fa31998aa98
Basti Tee <basti.tee@gmx.de>
2017-03-02 16:18:29 +0100

    dockerized setup;

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM alpine
+LABEL version="1.0"
+LABEL description="Docker image containing a minimal jekyll/ruby setup."
+LABEL maintainer="basti.tee@gmx.de"
+
+RUN apk upgrade --update \
+ && apk add libatomic readline readline-dev libxml2 libxml2-dev \
+        ncurses-terminfo-base ncurses-terminfo \
+        libxslt libxslt-dev zlib-dev zlib \
+        ruby ruby-dev yaml yaml-dev \
+        libffi-dev build-base git nodejs \
+        ruby-io-console ruby-irb ruby-json ruby-rake \
+ && gem install --no-document redcarpet kramdown maruku rdiscount RedCloth liquid pygments.rb \
+ && gem install --no-document sass safe_yaml \
+ && gem install --no-document jekyll -v 2.5 \
+ && gem install --no-document jekyll-paginate jekyll-sass-converter \
+ && gem install --no-document jekyll-sitemap jekyll-feed jekyll-redirect-from \
+ && rm -rf /root/src /tmp/* /usr/share/man /var/cache/apk/* \
+ && apk del build-base zlib-dev ruby-dev readline-dev \
+            yaml-dev libffi-dev libxml2-dev \
+ && apk search --update
+
+RUN mkdir /workdir
+EXPOSE 8000
+ENTRYPOINT ["jekyll"]
--- start.sh
+++ start.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-# cd to script's dir
-cd "$( dirname "$( readlink -f "$0" )")"
+function print() {
+    [ ! -z $( command -v tput ) ] && {
+        echo "$( tput setaf 1 )$@$( tput sgr0 )"
+    } || {
+        echo $@
+    }
+}
 
-# create copies of default files
+print "Preparing execution environment..."
+[ -z $( command -v docker ) ] && {
+  echo "Script requires docker."
+  exit 1
+}
+cd "$( dirname "$( readlink -f "$0" )")"
+DOCKER_IMG="alpine/jekyll-minimal:latest"
+CURRDIR="$( readlink -f $( pwd ))"
+print "Running content from $CURRDIR"
+
+print "Creating copies of default files..."
 for file in _posts _config.yml _includes/site-ext.html _includes/footer.html _includes/script.js
 do
   if [ -L "$file" ]
   then
-    echo "-- file $file present as symlink."
+    echo "File $file present as symlink."
   elif [ -e "$file" ]
   then
-    echo "-- file $file already present."
+    echo "File $file already present."
   else
-    echo "-- will copy $file from default."
+    echo "Will copy $file from default."
     cp -vr ${file}.default ${file}
   fi
 done
-echo
 
-# startup jekyll
-jekyll server --watch --host localhost --port 8000
+print "Building docker image..."
+docker build -t $DOCKER_IMG - < Dockerfile
 
-# cleanup
-rm -rf _site
+print "Running page using docker..."
+docker run --rm -v ${CURRDIR}:/workdir -p 8000:8000 alpine/jekyll-minimal \
+server -s /workdir -d /workdir/_site --port 8000 --host 0.0.0.0
