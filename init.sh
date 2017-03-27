fd8292798b3bca17662b9f7dc522314c1f31a577
Basti Tee <basti.tee@gmx.de>
2017-03-27 13:16:58 +0200

    removed docker references;

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$( dirname "$( readlink -f "$0" )")"
+CURRDIR="$( readlink -f $( pwd ))"
+echo "Initializing content from $CURRDIR"
+
+echo "Creating copies of default files..."
+for file in _posts _config.yml _includes/site-ext.html _includes/footer.html _includes/script.js
+do
+  if [ -L "$file" ]
+  then
+    echo "File $file present as symlink."
+  elif [ -e "$file" ]
+  then
+    echo "File $file already present."
+  else
+    echo "Will copy $file from default."
+    cp -vr ${file}.default ${file}
+  fi
+done
