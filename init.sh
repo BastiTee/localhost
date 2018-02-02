fe67d6113fe2797a2d9f0c0a370deabc1ee5ed86
Basti Tee <basti.tee@posteo.de>
2018-02-02 15:26:21 +0100

    removed-unused-tracking-references

--- init.sh
+++ init.sh
@@ -5,18 +5,18 @@ CURRDIR="$( readlink -f $( pwd ))"
 echo "Initializing content from $CURRDIR"
 
 echo "Creating copies of default files..."
-for file in _posts _config.yml _includes/site-ext.html _includes/footer.html _includes/script.js _includes/tracking.html
+for file in _posts _config.yml _includes/site-ext.html _includes/footer.html _includes/script.js
 do
-  if [ -L "$file" ]
-  then
-    echo "File $file present as symlink."
-  elif [ -e "$file" ]
-  then
-    echo "File $file already present."
-  else
-    echo "Will copy $file from default."
-    cp -vr ${file}.default ${file}
-  fi
+    if [ -L "$file" ]
+    then
+        echo "File $file present as symlink."
+elif [ -e "$file" ]
+    then
+        echo "File $file already present."
+    else
+        echo "Will copy $file from default."
+        cp -vr ${file}.default ${file}
+    fi
 done
 
 echo "Creating blog resource folder..."
