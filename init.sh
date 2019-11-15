c8a2ff21950ead7ac0cd160bad96eda24b29c50e
Basti Tee <basti.tee@posteo.de>
2019-11-15 09:01:06 +0100

    Make localhost self contained

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
-cd "$( dirname "$( readlink -f "$0" )")"
-CURRDIR="$( readlink -f $( pwd ))"
-echo "Initializing content from $CURRDIR"
+cd "$( cd "$( dirname "$0" )"; pwd )"
 
 echo "Creating copies of default files..."
-for file in _posts _config.yml _includes/site-ext.html _includes/footer.html _includes/script.js
+for file in \
+_posts \
+_config.yml \
+_includes/site-ext.html \
+_includes/footer.html \
+_includes/script.js
 do
-    if [ -L "$file" ]
-    then
+    if [ -L "$file" ]; then
         echo "File $file present as symlink."
-elif [ -e "$file" ]
-    then
+    elif [ -e "$file" ]; then
         echo "File $file already present."
     else
         echo "Will copy $file from default."
@@ -19,5 +19,19 @@ elif [ -e "$file" ]
     fi
 done
 
-echo "Creating blog resource folder..."
-mkdir -p res/blogres
+echo "Creating resource folder..."
+mkdir -vp res/blogres
+
+echo "Creating docker image..."
+docker build -t "basti-tee/jekyll" .
+docker run --rm \
+-v $(pwd):/workdir \
+-p 8000:8000 \
+"basti-tee/jekyll" \
+server \
+--source /workdir \
+--destination /workdir/_site \
+--layouts /workdir/_layouts \
+--plugins /workdir/_plugins \
+--port 8000 \
+--host 0.0.0.0
