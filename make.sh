541ba756a3f7c3f8fb328a32d62aa24e974fbbfe
Basti Tee <basti.tee@posteo.de>
2019-11-17 23:13:20 +0100

    Execute as user name

--- make.sh
+++ make.sh
@@ -25,13 +25,14 @@ Options:
     -c  DIR   Jekyll preview cache (default: ./_cache)
     -x  CMD   Jekyll command line (default: $DEFAULT_COMMAND)
     -u  URL   Overwrites site url (default: http://localhost:50600)
+    -w  USER  Executing username (defaukt: $USER)
     -g        Only generate target site
     -s        Skip docker image creation
 EOF
 exit 0
 }
 
-while getopts p:d:a:t:x:c:u:gsh opt
+while getopts p:d:a:t:x:c:u:w:gsh opt
 do
    case $opt in
        p) POSTS_FOLDER=$OPTARG;;
@@ -42,6 +43,7 @@ do
        x) COMMAND="$OPTARG";;
        g) GENERATE=1;;
        s) SKIP_DOCKER=1;;
+       w) USERNAME="$OPTARG";;
        u) SITE_URL="$OPTARG";;
        h) print_help ;;
        *) print_help ;;
@@ -56,6 +58,7 @@ TARGET_FOLDER=${TARGET_FOLDER:-$(pwd)/_site}
 SITE_URL=${SITE_URL:-http://localhost:50600}
 COMMAND=${COMMAND:-$DEFAULT_COMMAND}
 GENERATE=${GENERATE:-0}
+USERNAME=${USERNAME:-$USER}
 SKIP_DOCKER=${SKIP_DOCKER:-0}
 [ $GENERATE -eq 1 ] && COMMAND="build"
 cat << EOF
@@ -66,6 +69,7 @@ ASSETS FOLDER:  $ASSETS_FOLDER
 CACHE FOLDER:   $CACHE_FOLDER
 TARGET FOLDER:  $TARGET_FOLDER
 SITE_URL:       $SITE_URL
+USERNAME:       $USERNAME
 JEKYLL COMMAND: $COMMAND
 ---
 EOF
@@ -80,7 +84,7 @@ _config.yml > _config.yml.effective
 
 # Run dockerized jekyll
 docker run --rm -p 50600:50600 -p 50601:50601 \
--e LOCAL_USER_ID=`id -u $USER` \
+-e LOCAL_USER_ID=`id -u $USERNAME` \
 -v $(pwd)/_config.yml.effective:/home/user/jekyll/_config.yml \
 -v $(pwd)/index.md:/home/user/jekyll/index.md \
 -v $(pwd)/feed.xml:/home/user/jekyll/feed.xml \
