c8efb48f8893690b3a14e9724e3084b5b77df518
Basti Tee <basti.tee@posteo.de>
2019-11-19 20:35:13 +0100

    Entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ USER_ID=${LOCAL_USER_ID:-9001}
 getent passwd $USER_ID
 adduser -s /bin/sh -u $USER_ID user -D ||true
 echo "Starting with user: $USER_ID"
+echo "Executing jekyll $@"
 
 mkdir -p /usr/share/jekyll
 mkdir -p /usr/share/jekyll/_site
